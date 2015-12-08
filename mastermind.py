92d86fb092500a91f18e19f941ed718c471f3106
rc314159 <rc314159@users.noreply.github.com>
2015-12-08 01:35:33 -0500

    wrote some code

--- mastermind.py
+++ mastermind.py
@@ -1,2 +1,25 @@
 #Raymond Chaing
 #Mastermind
+
+#create randomized code
+a=[]
+i= [1,2,3,4]
+c=0
+#create list of 4 random letters from qwerty
+for i in range(0,4):
+	a.append(random.choice('qwerty'))
+	
+#enter inputs
+print "Welcome to Mastermind"
+print "Enter your inputs in this format"
+print ""qwer""# probably need to escape this
+user= input("Enter your guess:")
+guess=list(user)
+
+#while loop to compare code and user input
+#allow 10 tries
+while c<10:
+	
+
+#at 10 tries reset the code and counter and start the game over
+#give option to end the game
