d5d5845449739a2882b49a9d70918bc87e8d404b
rc314159 <rc314159@users.noreply.github.com>
2015-12-09 20:05:45 -0500

    revised algorithm

--- mastermind.py
+++ mastermind.py
@@ -5,36 +5,45 @@
 a=[]
 i= [1,2,3,4]
 c=0
+import random 
 #create list of 4 random letters from qwerty
 for i in range(0,4):
-	a.append(random.choice('qwerty'))
+    a.append(random.choice('qwerty'))
+print (a)
 	
 #enter inputs
-print "Welcome to Mastermind"
-print "Enter your inputs in this format"
-print ""qwer""# probably need to escape this
+print ("Welcome to Mastermind")
+print ("Enter your inputs without parentheses around them")
 user= input("Enter your guess:")
 guess=list(user)
 
 #while loop to compare code and user input
 #allow 10 tries
 while c<10:
-	for w in a:
-		if guess[w] == a[w]:
-			print "X"
-		else 
-			b=w+1
-			while b<4
-				if guess[b]== a[b]:
-					print "O"
-				b=b+1
-	if guess==a:
-		print "You Win"
-		c=10
-	c=c+1
-	print "Guess Again"
-	user= input("Enter your guess:")
-	guess=list(user)
+    for w in range(0,4):
+        if guess[w] == a[w]:
+            print ("X")
+            continue
+        else:
+            b=w+1
+            q=w-1
+            while b<4:
+                if guess[b]== a[b]:
+                    print ("O")
+                    break
+                b=b+1
+            while q>0:
+                if guess[q]== a[q]:
+                    print ("O")
+                    break
+                q=q-1
+    if guess==a:
+        print ("You Win")
+        break
+    c=c+1
+    print ("Guess Again")
+    user= input("Enter your guess:")
+    guess=list(user)
 
 
 #at 10 tries reset the code and counter and the game ends
